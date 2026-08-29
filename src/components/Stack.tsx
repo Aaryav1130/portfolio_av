6bbfadca08f6c7111792382356eac46a1b079c12
Aaryav Chaudhary <aaryav1130@gmail.com>
2026-08-29 10:44:46 +0530

    fix: resolve build errors for deployment

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { Icon, type IconName } from './Icons'
 import { SectionHead } from './Chrome'
 import { stack } from '../data/content'
